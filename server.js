3bfc9240b652b9610a584add0dc01550df70ff97
lquijano <quijano1012@gmail.com>
2018-08-26 17:53:04 -0700

    updates

--- server.js
+++ server.js
@@ -1,38 +1,35 @@
 // Dependencies requirements
 var express = require("express");
-var bodyParser = require("body-parser");
-var router = express.Router();
-var mysql = require("mysql");
-var bcrypt = require("bcryptjs");
-
-//session stuff
-var cookieParser = require("cookie-parser");
-
-var session = require("express-session");
-
-
-// //allow sessions
-// app.use(
-//   session({ secret: "app", cookie: { maxAge: 1 * 1000 * 60 * 60 * 24 * 365 } })
-// );
-//
-// app.use(cookieParser());
-
 // Express configuration
 var app = express();
+var bodyParser = require("body-parser");
+// var router = express.Router();
+var mysql = require("mysql");
+var bcrypt = require("bcryptjs");
+//session stuff
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+
+//allow sessions
+app.use(
+  session({ secret: "app", cookie: { maxAge: 1 * 1000 * 60 * 60 * 24 * 365 } })
+);
+
+app.use(cookieParser());
+
 var PORT = 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // use res.render to load up an ejs view file
 
-Routes testing
-app.get("/", function(req, res) {
-  res.send("<h1>Welcome to the route path</h1>");
-});
+// Routes testing
+// app.get("/", function(req, res) {
+//   res.send("<h1>Welcome to the route path</h1>");
+// });
 
-app.get('/', function(req, res) {
-	res.render('pages/index');
+app.get("/", function(req, res) {
+  res.render("pages/index");
 });
 
 // Listener, starting our server
