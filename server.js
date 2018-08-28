b68fd4979399f617069bd19083e2c56bf9c98cbf
lquijano <quijano1012@gmail.com>
2018-08-27 20:09:08 -0700

    created registrations mysql database to store users emails and passwords. Added the .post route within server.js to initiliaze sessions and store email

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 // Dependencies requirements
 var express = require("express");
 var bodyParser = require("body-parser");
-// Express configuration
-var app = express();
-var router = express.Router();
 var mysql = require("mysql");
 var bcrypt = require("bcryptjs");
-//session stuff
+// Express configuration
+//initializing sessions
 var cookieParser = require("cookie-parser");
-
 var session = require("express-session");
+
+var app = express();
+var router = express.Router();
+
 app.use(express.static("public"));
 //allow sessions
 // app.use(
@@ -18,11 +19,10 @@ app.use(express.static("public"));
 //
 // app.use(cookieParser());
 
-
 var PORT = 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // use res.render to load up an ejs view file
 
 //Routes testing
@@ -30,20 +30,31 @@ app.set('view engine', 'ejs');
 //   res.send("<h1>Welcome to the route path</h1>");
 // });
 
-app.get('/', function(req, res) {
-	res.render('pages/index');
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser.
+app.use(cookieParser());
+
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(
+  session({ secret: "app", cookie: { maxAge: 1 * 1000 * 60 * 60 * 24 * 365 } })
+);
+
+app.get("/login/:email/:password", function(req, res) {
+  res.render("pages/login");
 });
 
-app.get('/questionnaire', function(req, res) {
-	res.render('pages/questionnaire');
+app.get("/questionnaire", function(req, res) {
+  res.render("pages/questionnaire");
 });
 
-app.get('/meet', function(req, res) {
-	res.render('pages/meet');
+app.get("/meet", function(req, res) {
+  res.render("pages/meet");
 });
 
-app.get('/contact', function(req, res) {
-	res.render('pages/contact');
+app.get("/contact", function(req, res) {
+  res.render("pages/contact");
 });
 
 // Listener, starting our server
