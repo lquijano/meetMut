ef3a29d1d9dab406040b09fb5b498caf7836b977
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-09-04 13:30:39 -0700

    added a schema for the questions to work with the tutor that I may try implementing into the project if it ends up being easier to display the results that way

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,21 @@ CREATE TABLE users(
 	password_hash VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE questions(
+	id INT NOT NULL AUTO_INCREMENT,
+	q TEXT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE answers(
+	id INT NOT NULL AUTO_INCREMENT,
+	a TEXT NOT NULL,
+	score INT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+-- would we have the questions and answers in different tables since there are 4 answers to every question? the questions are already created in an object in questionnaire.js.
+
+-- How and where should we write the mysql query when the users score is tallied up?
+
